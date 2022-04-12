8d66a5daa183109d9ef9fc185e73b441c1440269
xlq <1135506045@qq.com>
2022-04-12 14:20:58 +0800

    【仍有误差】获得单幅图中心点的地理空间坐标，根据GDAL变换得到对应像素点的地理空间坐标再转换成拼接图中的经纬度

--- test_parper_get_ervery_latlon.py
+++ test_parper_get_ervery_latlon.py
@@ -5,6 +5,7 @@ import numpy as np
 import PIL.Image
 import PIL.ExifTags
 import pyexiv2
+from osgeo import gdal,osr
 
 #将经纬度信息转换为小数点形式
 def decimal_coords(coords, ref):
@@ -43,7 +44,61 @@ def get_xmp_info(img_path):
                     'FocalLength': focal_length,'ImageWidth': float(img_widh),'ImageHeight': float(img_height)
                   }
     return dist
+#根据经纬度得出地理空间坐标
+def lonlat2pixel(lon,lat):
+    img1_path = './images/GS.tif'
+    ds = gdal.Open(img1_path)
+    # 创建目标空间参考
+    srs = osr.SpatialReference()
+    srs.ImportFromWkt(ds.GetProjection())
+    srsLatLong = srs.CloneGeogCS()
+    ct = osr.CoordinateTransformation(srsLatLong, srs)
+    gt = ds.GetGeoTransform()
+    # x = 263853.4085 y = 4238056.654 通过经纬度获取地理空间坐标
+    # GDA3.0以后TransformPoint的参数为（lat, lon）而不是（lon, lat）
+    (X, Y, height) = ct.TransformPoint(lat, lon)
+    return (X, Y)
 
+def geoXY2latlon(xp, yp):
+    """
+       Returns latitude/longitude coordinates from pixel x, y coords
+
+       Keyword Args:
+         img_path: Text, path to tif image
+         x: Pixel x coordinates. For example, if numpy array, this is the column index
+         y: Pixel y coordinates. For example, if numpy array, this is the row index
+       """
+    img1_path = './images/GS.tif'
+    ds = gdal.Open(img1_path)
+    old_cs = osr.SpatialReference()
+    old_cs.ImportFromWkt(ds.GetProjectionRef())
+
+    # create the new coordinate system
+    # In this case, we'll use WGS 84
+    # This is necessary becuase Planet Imagery is default in UTM (Zone 15). So we want to convert to latitude/longitude
+    wgs84_wkt = """
+       GEOGCS["WGS 84",
+           DATUM["WGS_1984",
+               SPHEROID["WGS 84",6378137,298.257223563,
+                   AUTHORITY["EPSG","7030"]],
+               AUTHORITY["EPSG","6326"]],
+           PRIMEM["Greenwich",0,
+               AUTHORITY["EPSG","8901"]],
+           UNIT["degree",0.01745329251994328,
+               AUTHORITY["EPSG","9122"]],
+           AUTHORITY["EPSG","4326"]]"""
+    new_cs = osr.SpatialReference()
+    new_cs.ImportFromWkt(wgs84_wkt)
+
+    # create a transform object to convert between coordinate systems
+    transform = osr.CoordinateTransformation(old_cs, new_cs)
+    gt = ds.GetGeoTransform()
+    lat_lon = transform.TransformPoint(xp, yp)
+
+    lat = lat_lon[0]
+    lon = lat_lon[1]
+
+    return (lat,lon)
 if __name__ == "__main__":
     #读取两张图像
     img1_path = './images/DJI_20210803111304_0324_T.JPG'
@@ -59,10 +114,10 @@ if __name__ == "__main__":
     coords2 = (img2_xmp['Latitude'],img2_xmp['Longitude'])
     print('coords1:',coords1)
     print('coords2:',coords2)
+    #计算图像中心点对应的地理空间坐标系
+    (geo_x,geo_y)=lonlat2pixel(coords2[1],coords2[0])
+
     #每个像素对应的地面距离
-    #红外传感器宽度 像元间距 * 图像宽度 单位是mm
-    sendor_w_ir = 12  * img1_xmp['ImageWidth']/ 1000
-    #可见光传感器宽度 8.8 x 6.6mm
     # The camera sensor is 1/2.3 inch and image size is 6.3116*4.7492mm.
     sendor_w_vis = 6.3116
     sendor_h_vis = 4.7492
@@ -72,26 +127,19 @@ if __name__ == "__main__":
     gsd_vis_w = (sendor_w_vis * h ) / (float(img2_xmp['FocalLength']) * float(img2_xmp['ImageWidth']))
     gsd_vis_h = (sendor_h_vis * h) / (float(img2_xmp['FocalLength']) * float(img2_xmp['ImageHeight']))
 
-
+    gt = [geo_x, gsd_vis_w, 0,geo_y,0,gsd_vis_h]
     #中心点坐标
     center_point = np.array([img2.shape[1]/2,img2.shape[0]/2])
     #左上角坐标
-    left_point = np.array([1036.3905,852.8012])
+    left_point = np.array([img2.shape[1],img2.shape[0]])
 
     #像素差 距离中心点的实际距离
-
     sub = center_point - left_point
-    sub = sub * np.array([gsd_vis_w,gsd_vis_h])
-    offset_lat = 9.791212210578413e-06
-    offset_lon = 1.1660749935330442e-05
-    # 纬度
-    lat = coords2[0] - sub[1] *offset_lat
-    # 经度
-    lon = coords2[1] + sub[0] * offset_lon
-    # # 纬度
-    # lat = coords1[0] + (sub[1] / 30.9/3600)
-    # # 经度
-    # lon = coords1[1] + sub[0] / 23.6/3600
+    #获取目标的地理空格坐标系
+    target_geo_x = (sub[0] * gt[1]) + gt[0]
+    target_geo_y = (sub[1] * gt[5]) + gt[3]
+
+    (lat,lon) = geoXY2latlon(target_geo_x,target_geo_y)
     print(lat,lon)
     target_lat = 40.49333256
     target_lon = 95.70826548
