c688c1dd322545faf62d7e4d4e86da180eb51803
xlq <1135506045@qq.com>
2022-05-06 10:26:20 +0800

    基于H分量采用OTSU算法获取阈值范围

--- test_image_HSV_+seg.py
+++ test_image_HSV_+seg.py
@@ -1,19 +1,29 @@
-from skimage.color import rgb2hsv
+from skimage.color import rgb2hsv,rgb2gray
 import cv2
 import numpy as np
+from skimage.filters import threshold_otsu
 #convert to hsv scale
 import matplotlib.pyplot as plt
-img2_path = './images/DJI_20210803111020_0285_W.JPG'
+img2_path = './result/output_crop_raster_0067.tif'
 sample = cv2.imread(img2_path)
 sample_h= rgb2hsv(sample)
+sample_g = rgb2gray(sample)
+Hue_threshold = threshold_otsu(sample_h[:,:,0])
+sample_bw = sample_g < Hue_threshold
+red = sample[:,:,0]*sample_bw
+green = sample[:,:,1]*sample_bw
+blue = sample[:,:,2]*sample_bw
+otsu_mask = np.dstack((red,green,blue))
 #graph per HSV Channel
-fig, ax = plt.subplots(1, 3, figsize=(15,5))
+fig, ax = plt.subplots(1, 4, figsize=(15,5))
 ax[0].imshow(sample_h[:,:,0], cmap='hsv')
 ax[0].set_title('Hue',fontsize=15)
 ax[1].imshow(sample_h[:,:,1], cmap='hsv')
 ax[1].set_title('Saturation',fontsize=15)
 ax[2].imshow(sample_h[:,:,2], cmap='hsv')
 ax[2].set_title('Value',fontsize=15);
+ax[3].imshow(otsu_mask)
+ax[3].set_title('OTSU mask',fontsize=15);
 plt.show()
 fig, ax = plt.subplots(1,3,figsize=(15,5))
 im = ax[0].imshow(sample_h[:,:,0],cmap='hsv')
